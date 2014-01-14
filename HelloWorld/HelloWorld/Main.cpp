c6af96d61737cd517f47320e1d972b4cf1168eab
SLJoosten <sljoosten@outlook.com>
2014-01-14 12:13:21 -0600

    Initial commit

---
+++ HelloWorld/HelloWorld/Main.cpp
@@ -0,0 +1,13 @@
+#include <conio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World" << endl;
+	_getch();
+	cout << "Goodbye!" << endl;
+	_getch();
+	return 1;
+}
