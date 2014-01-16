b7c28b88b98efd8268bc332cfc0bf120c9df325f
SLJoosten <sljoosten@outlook.com>
2014-01-16 11:52:04 -0600

    Completed cout application for assignment 2
    used cout along with endl and \t to display a verse on the console

---
+++ CoutPoetry/CoutPoetry/Main.cpp
@@ -0,0 +1,16 @@
+#include <conio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	//display verse using cout, using new lines and indents accordingly
+	cout << "Computers, computers everywhere" << endl;
+	cout << "\tas far as I can C." << endl;
+	cout << "I really, really like these things." << endl;
+	cout << "\tOh joy. Oh joy for me!" << endl;
+	//wait for user to press a button
+	_getch();
+	return 1;
+}
