702c42d14ef54fd4367f15c0c90b9e3665c4e2f3
SLJoosten <sljoosten@outlook.com>
2014-01-16 11:22:07 -0600

    Completed output application
    used cout with integer variable on same line

--- Output/Output/Main.cpp
+++ Output/Output/Main.cpp
@@ -5,8 +5,14 @@ using namespace std;
 
 int main()
 {
-	cout << "Working!";
+	//declare num variable
+	int num = 28;
 
+	//use cout to write to the console
+	cout << "The number is: " << num << "." << endl;
+	cout << "Working!" << endl;
+	cout << "\tthis is an output statement" << endl;
+	//wait for user to press a key
 	_getch();
 	return 1;
 }
