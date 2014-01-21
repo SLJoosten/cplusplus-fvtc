c046f0ec0373aaecc0a5fc71e461b9134b777c09
SLJoosten <sljoosten@outlook.com>
2014-01-21 11:47:43 -0600

    Completed input with tabs for assignment 3

---
+++ OutputTabs/OutputTabs/Main.cpp
@@ -0,0 +1,15 @@
+#include <conio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "PART NO.\t" << "PRICE" << endl;
+	cout << "T1267\t        " << "$6.34" << endl;
+	cout << "T1300\t        " << "$8.92" << endl;
+	cout << "T2401\t       " << "$65.40" << endl;
+	cout << "T4482\t      " << "$103.45" << endl;
+	_getch();
+	return 1;
+}
