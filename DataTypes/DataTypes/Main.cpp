dc60fd0cb9e279a7727a1e8ccc4a5a6cc2db71a4
SLJoosten <sljoosten@outlook.com>
2014-01-21 11:29:44 -0600

    Output different variables

--- DataTypes/DataTypes/Main.cpp
+++ DataTypes/DataTypes/Main.cpp
@@ -1,14 +1,25 @@
 #include <conio.h>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-	int num = 5;
+	int num = -5;
+	int num2 = 27;
 	bool success = true;
+	double miles = 5.256;
+	string hello = "hi there";
+	char c = 'y';
 
-	cout << "I'm working!" << endl;
+	cout << "num = " << num << endl;
+	cout << "success = " << success << endl;
+	cout << "miles = " << miles << endl;
+	cout << "hello = " << hello << endl;
+	cout << "c = " << c << endl;
+	cout << "num + num2 = " << num + num2 << endl;
+
 	_getch();
 	return 1;
 }
