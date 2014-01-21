147d0de801606c5003eecfc884edb9b536cd68fc
SLJoosten <sljoosten@outlook.com>
2014-01-21 11:11:46 -0600

    Initial check-in DataTypes

---
+++ DataTypes/DataTypes/Main.cpp
@@ -0,0 +1,14 @@
+#include <conio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int num = 5;
+	bool success = true;
+
+	cout << "I'm working!" << endl;
+	_getch();
+	return 1;
+}
