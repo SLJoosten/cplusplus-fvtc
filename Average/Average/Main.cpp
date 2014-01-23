d89feba7d8c2bdb3937a6182de13fcbc38df1e76
SLJoosten <sljoosten@outlook.com>
2014-01-23 11:32:44 -0600

    Initial check-in Average

---
+++ Average/Average/Main.cpp
@@ -0,0 +1,16 @@
+#include <conio.h>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	double firstnum;
+	double secnum;
+	double thirdnum;
+	double total;
+	double average;
+	_getch();
+	return 0;
+}
--- Sums/Sums/Main.cpp
+++ Sums/Sums/Main.cpp
@@ -1,11 +1,32 @@
 #include <conio.h>
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 int main()
 {
-	cout << "Working!";
+	cout << "Working!"<< endl;
+
+	double number1 = 3.678976905, number2 = 7, number3 = 11;
+	double sum = 0, average = 0, product = 0, exponent = 0, modulus = 0;
+
+	product = number1 * number2 * number3;
+	exponent = pow(2, 4);
+	sum = number1 + number2 + number3;
+	average = sum / 3;
+	modulus = 5 % 2;
+
+	cout << "Number 1 = " << number1 << endl;
+	cout << "Number 2 = " << number2 << endl;
+	cout << "Number 3 = " << number3 << endl;
+	cout << setprecision(2) << fixed;
+	cout << "Sum = " << sum << endl;
+	cout << "Average = " << average << endl;
+	cout << "Product = " << product << endl;
+	cout << "Exponent = " << exponent << endl;
+	cout << "Modulus = " << modulus << endl;
+
 	_getch();
-	return 1;
+	return 0;
 }
