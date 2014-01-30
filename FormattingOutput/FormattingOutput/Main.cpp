75341204534e330d3f1ece84e17ec136f5b66e0c
SLJoosten <sljoosten@outlook.com>
2014-01-30 12:28:51 -0600

    Added hexadecimal and octal conversions

--- FormattingOutput/FormattingOutput/Main.cpp
+++ FormattingOutput/FormattingOutput/Main.cpp
@@ -6,6 +6,12 @@ using namespace std;
 
 int main()
 {
+	int integer_num = 32;
+	double double_num = 42.98765;
+
+	cout << "" << endl;
+	cout << "The hex of " << integer_num << " is " << hex << integer_num << dec << endl;
+	cout << "The octal of " << integer_num << " is " << oct << integer_num << endl;
 
 	_getch();
 	return 0;
