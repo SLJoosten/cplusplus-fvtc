1a9cdb98ddc226a11e4cd76c1e74fc27ba1b0524
SLJoosten <sljoosten@outlook.com>
2014-02-04 12:17:51 -0600

    Completed Formatting Output

--- FormattingOutput/FormattingOutput/Main.cpp
+++ FormattingOutput/FormattingOutput/Main.cpp
@@ -11,6 +11,11 @@ int main()
 	//declare variables
 	int integer_num = 32;
 	double double_num = 42.98765;
+	double column_width = 10;
+	double column_width_small = 5;
+
+	/*changing double_num to a float results in rounding error when the number is rounded to 6 decimal places 
+	due to being a less precise data type (though faster)*/
 
 	//blank line followed by hexadecimal and octal conversions on their own lines
 	cout << "" << endl;
@@ -19,11 +24,16 @@ int main()
 
 	//blank line followed by double formatted to multiple decimal places
 	cout << "" << endl;
+	cout << fixed << right << "|" << setw(column_width) << setprecision(3) << double_num << "|" << endl;
+	cout << right << "|" << setw(column_width) << setprecision(4) << double_num << "|" << endl;
+	cout << right << "|" << setw(column_width) << setprecision(5) << double_num << "|" << endl;
+	cout << right << "|" << setw(column_width) << setprecision(6) << double_num << "|" << endl;
 
 	//blank line followed by final formatted display on single line
 	cout << "" << endl;
+	cout << "|" << right << setw(column_width_small) << integer_num << "|" << setw(column_width) << setprecision(2) << double_num << "|" << endl;
 
-
+	//need to wait for keystroke and return a value
 	_getch();
 	return 0;
 }
