fddbde0b4dc6abbed1bc79869b761016fdc50699
SLJoosten <sljoosten@outlook.com>
2014-02-27 11:45:10 -0600

    Completed TryParse
    in class 2/27

---
+++ TryParse/TryParse/Main.cpp
@@ -0,0 +1,48 @@
+#include <conio.h>
+#include <iostream>
+#include <string>
+#include "..\..\HelperClass\HelperClass\HelperBot.h"
+
+using namespace std;
+
+bool TryParse(string, int &);
+
+int main()
+{
+	bool quit = false;
+
+	while (!quit)
+	{
+		cout << "Please enter a number: ";
+		string user_input = "";
+		int number = 0;
+		cin >> user_input;
+
+		if (TryParse(user_input, number))
+		{
+			cout << endl << "You entered: " << number << endl;
+		}
+		else
+		{
+			cout << endl << "That is not a number" << endl;
+		}
+
+		char c = _getch();
+		if (c == 'q' || c == 'Q')
+		{
+			quit = true;
+		}
+	}
+	
+	return 0;
+}
+
+bool TryParse(string input, int & value)
+{
+	bool success = HelperBot::IsNumeric(input);
+	if (success)
+	{
+		value = HelperBot::ConvertToInt(input);
+	}
+	return success;
+}
